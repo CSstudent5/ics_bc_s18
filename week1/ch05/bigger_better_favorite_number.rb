660c76dbb0ca5733da8b8ab801502f561001ce29
CSstudent5 <travis.carroll312@gmail.com>
2018-06-15 12:32:49 -0700

    Complete bigger_better_favorite_number.rb exercise

--- week1/ch05/bigger_better_favorite_number.rb
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -1,3 +1,3 @@
-puts "Hello, what's your favorite number?"
+puts "Howdy, what's your favorite number?"
 favorite_number = gets.to_i
 puts "Well, " + (favorite_number + 1).to_s + " is a greater number in terms of real number value, so it might be a more suitable favorite number."
