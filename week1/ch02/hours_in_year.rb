246561845f5ccd658a787d92dc160c136229705d
CSstudent5 <travis.carroll312@gmail.com>
2018-06-15 12:24:20 -0700

    Complete hours_in_year.rb exercise

--- week1/ch02/hours_in_year.rb
+++ week1/ch02/hours_in_year.rb
@@ -1,4 +1,4 @@
 puts "Hello, what's your name?"
 name = gets.chomp
 puts "It's nice to meet you " + name + ". Did you know that there were " + (365*24).to_s + " hours in a year?"
-puts "However, there are " + (366*24).to_s + " hours in a leap year!"
+puts "What's even cooler is there are " + (366*24).to_s + " hours in a leap year!"
