96e9ca5f92084751d86919b92cd51367bbdc6f39
CSstudent5 <travis.carroll312@gmail.com>
2018-07-06 19:56:47 -0700

    Complete sort.rb

--- week4/ch10/sort.rb
+++ week4/ch10/sort.rb
@@ -3,7 +3,28 @@ def sort(some_array) # This "wraps" recursive_sort.
 end
 
 def recursive_sort(unsorted_array, sorted_array)
-  # Your fabulous code goes here.
+    #looping through array to find the smallest word
+    #repeat sorting and pushing into sorted array
+    smallest = unsorted_array.reduce { |memo, word|  memo < word ? memo : word}
+  	unsorted_array.delete_at(unsorted_array.index(smallest))
+  	sorted_array << smallest
+  	#if unsorted array length > 0, repeat, else return sorted array
+  	if unsorted_array.length > 0
+  		recursive_sort(unsorted_array, sorted_array)
+  	else
+  		sorted_array
+  	end
 end
 
-sort [__, __, ...]
+puts 'Please enter words into the array on an empty line.'
+puts 'Hit "enter" on an empty line to quit inputting words.'
+#input array set-up
+index = 0;
+input = "entry";
+array_inputted = [];
+while input != ""
+  input = gets.chomp
+  array_inputted[index] = input
+  index = index + 1
+end
+puts sort(array_inputted)
